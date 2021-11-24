808c8d2f566b99d0ad4fa595a6ef485036369bcb
Camilo Martinez <caemartinezfo@unal.edu.co>
2021-11-24 14:38:30 -0500

    Creación de landing público

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Login from 'pages/Login';
 import Registro from 'pages/Registro';
 import Index from 'pages/Index';
 
+
 function App() {
   return (
       <Router>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import project from 'media/projectmanagement.jpg'
 
-// Alinear a la izquierda y que se corran solas 3 imágenes, Descripción al lado izquierdo con el botón
-// de inicio de sesión 
-
 const Index = () => {
+
     return (
-        <div>
-            <img src={project} alt='project' width='500px' style={{display:"block", margin:"auto", paddingTop:"50px"}}/>
+        <div style={{width:"1330px",height:"430px",overflow:"hidden",margin:"10px",position:"relative"}}>
+            <div >
+                <img src={project} alt='imgFondo' style={{position:"absolute",left:"-100%",right:"-100%",top:"-100%",bottom:"-100%",margin:"auto",minHeight:"100%",minWidth:"100%"}}/>
+            </div>
+            <div style={{background:"gray",borderRadius:"50px" ,opacity:"0.8", width:"900px",height:"200px", position:"relative", top:"100px",left:"190px", margin:"0"}}>
+            </div>
+            <h1 style={{position:"relative",top:"-80px", left:"220px",color:"white"}}>Bienvenido al sistema de gestión de proyectos</h1>
+            <span style={{position:"relative",color:"white",top:"-100px", left:"220px"}}><b>_______________________________________________________________________________________________________________________________</b></span>            
+            <p style={{position:"relative", color:"white", top:"-90px", left:"290px"}}><b>Todo para que puedas hacer seguimiento, evaluación y estado de tus proyectos académicos</b></p>
         </div>
+
+        
 
     )
 }
