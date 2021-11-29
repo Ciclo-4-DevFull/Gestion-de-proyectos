c8e3972f2669adae593ba9a66d8b646ef98b307d
Camilo Martinez <caemartinezfo@unal.edu.co>
2021-11-29 00:33:49 -0500

    Finalización de página de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,60 +1,36 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import google from 'media/google2.png'
 import project from 'media/projectmanagement.jpg'
 
 const login = () => {
     return (
-        <div style={{height:"66vh"}} className='overflow-hidden relative mx-1'>
-            <div >
-                <img src={project} alt='imgFondo' style={{position:"absolute",left:"-100%",right:"-100%",top:"-100%",bottom:"-100%",margin:"auto",minHeight:"100%",minWidth:"100%"}}/>
-            </div>
-            <div style={{background:"gray",borderRadius:"50px" ,opacity:"0.9", width:"400px",height:"400px", position:"relative", top:"20px",left:"450px", margin:"0"}}>
-            </div>
-            <h2 style={{position:"relative",top:"-370px", left:"550px",color:"white"}}>Iniciar sesión</h2>
-            <span style={{position:"relative",color:"white",top:"-390px", left:"500px"}}><b>_____________________________________________</b></span>            
-            <form style={{position:"relative",color:"white",top:"-370px", left:"480px"}}>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Correo electrónico:</label>
-                    <input type="email" className="form-control" aria-describedby="emailHelp" style={{width:"300px", height:"30px"}} />
-                </div>
-                <div className="mb-3" style={{paddingTop: '1vh'}}>
-                    <label for="exampleInputPassword1" className="form-label">Contraseña:</label>
-                    <input type="password" className="form-control" style={{width:"300px", height:"30px"}}/>
-                </div>
-                <div>
-                    <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" for="exampleCheck1">Recuérdame</label>
+        <div className='bg-cover pt-12' style={{height:"66vh", backgroundImage:`url(${project})`}}>
+            <div className='bg-opacity-90 bg-gray-400 w-80 m-auto p-3 rounded-2xl border-gray-500 border-2'>
+                <h3 className='text-white text-center'>Iniciar sesión</h3>
+                <div className='p-1' style={{ borderTop: "2px solid #fff "}}></div>
+                <form className='w-70'>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label text-white">Correo electrónico:</label>
+                        <input type="email" className="form-control h-8" aria-describedby="emailHelp"/>
                     </div>
-                    <label style={{color: 'white', textDecoration: 'underline', cursor: 'pointer', paddingBottom:"5px"}}>
-                        Olvidé la contraseña
-                    </label>
-                </div>
-                <div>
-                    <Link to="/bienvenida">
-                        <button type="submit" className="btn btn-primary" style={{marginBottom: '8px', backgroundColor:  '#426E86'}}>
-                            Ingresar
-                        </button>
-                    </Link>
-                </div>
-            </form>
-            <div style={{position:"relative",color:"white",top:"-370px", left:"480px"}}>
-                <label>¿No tienes cuenta?</label>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label text-white">Contraseña:</label>
+                        <input type="password" className="form-control h-8"/>
+                    </div>
+                    <div>
+                        <Link to="/bienvenida">
+                            <button type="submit" className="btn btn-primary p-1 w-20 bg-blue-400 mb-2">
+                                Ingresar
+                            </button>
+                        </Link>
+                    </div>
+                </form>
+                <label className='text-white mr-2'>¿No tienes cuenta?</label>
                 <Link to="/registro">
-                    <label style={{color: 'white', textDecoration: 'underline', cursor:'pointer'}}>
-                        Registrarse
+                    <label className='text-white underline cursor-pointer'>
+                        Regístrate
                     </label>
                 </Link>
-            </div>
-            <span style={{position:"relative",color:"white",top:"-370px", left:"480px"}}>
-                ---------------------<b> o </b>----------------------
-            </span>
-            <div style={{position:"relative",color:"white",top:"-370px", left:"480px"}}>
-                <button type="button" className="btn btn-light">
-                    <img src={google} alt="Google" style={{height: '4vh', marginRight: '2vh'}}/>
-                    Ingresar con Google
-                </button>
             </div>
         </div>
     )
