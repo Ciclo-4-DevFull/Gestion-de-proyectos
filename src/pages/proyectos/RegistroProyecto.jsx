393e070e824692c7e64903375fc401532cd973b9
John G <jhon_gonzalez04@hotmail.com>
2021-12-01 17:33:14 -0500

    Terminar pagina para registro de proyectos

--- src/pages/proyectos/RegistroProyecto.jsx
+++ src/pages/proyectos/RegistroProyecto.jsx
@@ -2,71 +2,77 @@ import React from 'react'
 
 const RegistroProyecto = () => {
     return (
-  <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="bg-white w-full shadow rounded">
+        <h3 class= "text-2xl font-bold leading-7 text-center">NUEVO PROYECTO</h3>
+        <div class="md:flex items-center mt-12">
+                <div class="w-full md:w-1/2 flex flex-col md:ml-6">
+                    <label class="font-semibold leading-none">ID Proyecto</label>
+                    <input type="text" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                </div>
+                <div class="w-full md:w-1/2 flex flex-col ml-6 mr-6">
+                    <label class="font-semibold leading-none">Nombre Proyecto</label>
+                    <input type="email" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                </div>
+        </div>
+        <div class="md:flex items-center mt-12">
+            <div class="w-full flex flex-col mt-8 ml-6 mr-6">
+                <label class="font-semibold leading-none">Objetivos Generales</label>
+                <textarea type="text" class="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
+            </div>
+            <div class="w-full flex flex-col mt-8 ml-6 mr-6">
+                <label class="font-semibold leading-none">Objetivos Especificos</label>
+                <textarea type="text" class="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
+            </div>
+        </div>
+        <div class="flex items-center justify-center w-full">
+            <button class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                 Guardar Proyecto
+            </button>
+        </div>
+        
+  {/* <div class="md:grid md:grid-cols-3 md:gap-6">
     <div class="md:col-span-1">
       <div class="px-4 sm:px-0">
-        <h3 class="text-lg font-medium leading-6 text-gray-900">NUEVO PROYECTO</h3>
-      </div>
-    </div>
-    <div class="mt-5 md:mt-0 md:col-span-2">
-      <form action="#" method="POST">
-        <div class="shadow overflow-hidden sm:rounded-md">
-          <div class="px-4 py-5 bg-white sm:p-6">
-            <div class="grid grid-cols-6 gap-6">
-              <div class="col-span-6 sm:col-span-3">
-                <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
-                <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
-
-              <div class="col-span-6 sm:col-span-3">
-                <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
-                <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
-
-              <div class="col-span-6 sm:col-span-4">
-                <label for="email_address" class="block text-sm font-medium text-gray-700">Email address</label>
-                <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
-
-              <div class="col-span-6 sm:col-span-3">
-                <label for="country" class="block text-sm font-medium text-gray-700">Country / Region</label>
-                <select id="country" name="country" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
-                </select>
-              </div>
-
-              <div class="col-span-6">
-                <label for="street_address" class="block text-sm font-medium text-gray-700">Street address</label>
-                <input type="text" name="street_address" id="street_address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
-
-              <div class="col-span-6 sm:col-span-6 lg:col-span-2">
-                <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                <input type="text" name="city" id="city" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
-
-              <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="state" class="block text-sm font-medium text-gray-700">State / Province</label>
-                <input type="text" name="state" id="state" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
-
-              <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                <label for="postal_code" class="block text-sm font-medium text-gray-700">ZIP / Postal</label>
-                <input type="text" name="postal_code" id="postal_code" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-              </div>
+   
+   
+                <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
+                        <p class="text-3xl font-bold leading-7 text-center">Contact me</p>
+                        <form action="" method="post">
+                            <div class="md:flex items-center mt-12">
+                                <div class="w-full md:w-1/2 flex flex-col">
+                                    <label class="font-semibold leading-none">Name</label>
+                                    <input type="text" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                                </div>
+                                <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
+                                    <label class="font-semibold leading-none">Phone</label>
+                                    <input type="email" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                                </div>
+                            </div>
+                            <div class="md:flex items-center mt-8">
+                                <div class="w-full flex flex-col">
+                                    <label class="font-semibold leading-none">Subject</label>
+                                    <input type="text" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                                </div>
+                                
+                            </div>
+                            <div>
+                                <div class="w-full flex flex-col mt-8">
+                                    <label class="font-semibold leading-none">Message</label>
+                                    <textarea type="text" class="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
+                                </div>
+                            </div>
+                            <div class="flex items-center justify-center w-full">
+                                <button class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                                    Send message
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Save
-            </button>
-          </div>
-        </div>
-      </form>
+      </div> */}
     </div>
-  </div>
+
 
 
     )
